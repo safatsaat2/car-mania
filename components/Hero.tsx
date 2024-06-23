67083b26d65968bf08cb9d0e33a41443e2b1f409
unknown <sakibsafat47@gmail.com>
2024-06-23 10:59:28 -0700

    hero implementation and css global implementation

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Effortlessly Rent or Reserve Your Ideal Car Today
+        </h1>
+        <p className="hero__subtitle">
+        Simplify Your Car Rental Experience with Our Seamless Booking Process
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
