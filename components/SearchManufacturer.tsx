4693366567d7908c072236b106d22006f6b7514e
unknown <sakibsafat47@gmail.com>
2024-06-24 12:32:13 -0700

    car catalogue implementation on searchbar

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -14,7 +14,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: searchManufacturerP
     ))
     return (
         <div className='search-manufacturer'>
-            <Combobox>
+            <Combobox value={manufacturer} onChange={setManufacturer}>
                 <div className="relative w-full">
                     <ComboboxButton className="absolute top-[14px]">
                         <Image src="/car-logo.svg" width={20} height={20} className='ml-4' alt='Car Logo' />
@@ -26,7 +26,25 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: searchManufacturerP
                             (
                                 filteredManufacturers.map((item)=>(
                                     <ComboboxOption value={item} key={item} className={({active})=>`relative search-manufacturer__option ${active ?'bg-primary-blue text-white': 'text-gray-900'}`}>
-                                        {item}
+                                        {({selected, active})=>(
+                                            <>
+                                            <span
+                                              className={`block truncate ${
+                                                selected ? 'font-medium' : 'font-normal'
+                                              }`}
+                                            >
+                                              {item}
+                                            </span>
+                                            {selected ? (
+                                              <span
+                                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                  active ? 'text-white' : 'text-teal-600'
+                                                }`}
+                                              >
+                                              </span>
+                                            ) : null}
+                                          </>
+                                        )}
                                     </ComboboxOption>
                                 ))
                             )
