6f73fd531073ee814c30efe26b1ad30061b91830
unknown <sakibsafat47@gmail.com>
2024-06-25 10:18:21 -0700

    set params

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import SearchManufacturer from './SearchManufacturer';
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -17,12 +18,40 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
 const SearchBar = () => {
 const [manufacturer, setManufacturer] =useState("");
-    const handleSearch = () => {
-
-    }
     const [model, setModel] = useState("");
-
-
+    const router = useRouter();
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    
+        if (manufacturer.trim() === "" && model.trim() === "") {
+          return alert("Please provide some input");
+        }
+    
+        updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+      };
+      const updateSearchParams = (model: string, manufacturer: string) => {
+        // Create a new URLSearchParams object using the current URL search parameters
+        const searchParams = new URLSearchParams(window.location.search);
+    
+        // Update or delete the 'model' search parameter based on the 'model' value
+        if (model) {
+          searchParams.set("model", model);
+        } else {
+          searchParams.delete("model");
+        }
+    
+        // Update or delete the 'manufacturer' search parameter based on the 'manufacturer' value
+        if (manufacturer) {
+          searchParams.set("manufacturer", manufacturer);
+        } else {
+           searchParams.delete("manufacturer");
+        }
+    
+        // Generate the new pathname with the updated search parameters
+        const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+    
+        router.push(newPathname);
+      };
     return (
         <form onSubmit={handleSearch} className='searchbar'>
             <div className="searchbar__item">
